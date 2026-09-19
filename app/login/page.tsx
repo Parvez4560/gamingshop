a626d1f06bf4adf50f0ba36a27efb799552652cf
Parvez Mia <salafipay@gmail.com>
2026-09-19 10:47:15 +0600

    Update website

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -54,12 +54,21 @@ export default function LoginPage() {
               className="bg-gray-100 border-none my-1 py-3 px-4 text-xs rounded-lg w-full outline-none text-gray-900"
             />
 
-            <input
-              type="email"
-              placeholder="Email"
-              required
-              className="bg-gray-100 border-none my-1 py-3 px-4 text-xs rounded-lg w-full outline-none text-gray-900"
-            />
+            <div className="relative my-1 w-full">
+  <input
+    type="email"
+    id="unique-id-here"
+    placeholder=" "
+    required
+    className="peer bg-gray-100 border-none pt-5 pb-2 px-4 text-xs rounded-lg w-full outline-none text-gray-900 placeholder-transparent"
+  />
+  <label
+    htmlFor="unique-id-here"
+    className="absolute text-gray-500 text-xs duration-300 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3 pointer-events-none"
+  >
+    Email
+  </label>
+</div>
 
             <input
               type="password"
